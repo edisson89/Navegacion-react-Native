20eb40388bf21ff49a42c1396e5a320e6af25a8e
edisson <edisson.12@hotmail.com>
2022-05-17 20:48:59 -0500

    Add files via upload

---
+++ src/theme/appTheme.tsx
@@ -0,0 +1,28 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    globlalMargin: {
+        marginHorizontal:  20,
+    },
+    title:{
+        fontSize: 30,
+        marginBottom: 5,
+
+    },
+    botonGrande:{
+        width: 100,
+        height: 100,
+        backgroundColor:'red',
+        borderRadius: 20,
+        alignItems: 'center',
+        justifyContent:'center',
+        marginRight: 10,
+        
+    },
+    botonGrandeTexto:{
+        color:'white',
+        fontSize:20,
+        fontWeight: 'bold',
+    }
+     
+});
